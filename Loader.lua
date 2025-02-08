ad926b6bddfb30d738952668773ed50c5d64bf94
ElevatorDark HUB <93924562+x2Kotaro@users.noreply.github.com>
2025-02-08 10:54:21 +0700

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,7 +1,5 @@
 local PlaceId = game.PlaceId
-if PlaceId == 83258181266838 then
-    loadstring(game:HttpGet('https://raw.githubusercontent.com/x2Kotaro/Venuz-hub/main/Planets.lua'))()
-elseif PlaceId == 76455837887178 then
+if PlaceId == 76455837887178 then
     loadstring(game:HttpGet('https://raw.githubusercontent.com/x2Kotaro/Venuz-hub/main/Digit.lua'))()
 else
     game.Players.LocalPlayer:Kick("game not support!")
